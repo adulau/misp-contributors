17f2188359270bb4817dcab81f5ebc8a8c79c637
Alexandre Dulaunoy <a@foo.be>
2018-02-28 11:45:11 +0100

    fix: add a topcommit accounting all the commits per user

--- contributors.py
+++ contributors.py
@@ -17,11 +17,11 @@ for r in gh.iter_repos():
     for cstat in r.iter_contributor_statistics():
         redcon.zincrby('r:{}'.format(r.name), cstat.author, cstat.total)
         print (r.name," ", cstat.total , " ",  cstat.author)
+        redcon.zincrby('topcommit', cstat.author, cstat.total)
     for c in r.iter_contributors():
         contributor = str(c)
         user = gh.user(contributor)
         redcon.set('a:{}'.format(user),user.avatar_url)
         redcon.sadd('users', user)
         redcon.zincrby('topversatile', contributor, 1)
-
 
