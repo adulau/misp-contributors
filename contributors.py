36452b7d686e51d6fffa902b520f924f72184fae
Alexandre Dulaunoy <a@foo.be>
2018-08-04 13:19:20 +0200

    fix: statistics API from GitHub is giving 202 when statistics are not generated and github3.py last_status interface is buggy. I introduced a quick hack to trigger the statistics for all the required repositories in advance. Maybe it will be useful to open an issue or fix github3 library to make this cleaner and remove this hack in the future...

--- contributors.py
+++ contributors.py
@@ -1,24 +1,39 @@
 import github3
 import json
 import redis
+import time
 
 from config import username, password
 gh = github3.login(username, password=password)
 
+print (gh)
 contributors = {}
 
 redcon = redis.StrictRedis(host='localhost', port=6379, db=10)
 redcon.flushdb()
 
-for r in gh.iter_repos():
+for r in gh.repositories():
+    if str(r.owner).find('MISP') >= 0:
+        print (r.owner)
+        try:
+            for x in r.contributor_statistics():
+                print ('trigger stats for {}'.format(r))
+        except:
+            continue
+        else:
+            continue
+
+
+for r in gh.repositories():
     if str(r.owner).find('MISP',) == -1:
         continue
     redcon.sadd('repositories', r.name)
-    for cstat in r.iter_contributor_statistics():
-        redcon.zincrby('r:{}'.format(r.name), cstat.author, cstat.total)
-        print (r.name," ", cstat.total , " ",  cstat.author)
-        redcon.zincrby('topcommit', cstat.author, cstat.total)
-    for c in r.iter_contributors():
+    for cstatfull in r.contributor_statistics():
+        print (cstatfull)
+        redcon.zincrby('r:{}'.format(r.name), cstatfull.author, cstatfull.total)
+        print (r.name," ", cstatfull.total , " ",  cstatfull.author)
+        redcon.zincrby('topcommit', cstatfull.author, cstatfull.total)
+    for c in r.contributors():
         contributor = str(c)
         user = gh.user(contributor)
         redcon.set('a:{}'.format(user),user.avatar_url)
