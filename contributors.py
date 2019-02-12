15bdaf0653bacae0cccc32e8775843632bc36819
Alexandre Dulaunoy <a@foo.be>
2019-02-12 22:37:30 +0100

    chg: [contributor] fixed the use of the github3 library

--- contributors.py
+++ contributors.py
@@ -9,7 +9,7 @@ gh = github3.login(username, password=password)
 print (gh)
 contributors = {}
 
-redcon = redis.StrictRedis(host='localhost', port=6379, db=10)
+redcon = redis.StrictRedis(host='localhost', port=6379, db=10, encoding=u'utf-8')
 redcon.flushdb()
 
 for r in gh.repositories():
@@ -29,14 +29,15 @@ for r in gh.repositories():
         continue
     redcon.sadd('repositories', r.name)
     for cstatfull in r.contributor_statistics():
-        print (cstatfull)
-        redcon.zincrby('r:{}'.format(r.name), cstatfull.author, cstatfull.total)
-        print (r.name," ", cstatfull.total , " ",  cstatfull.author)
-        redcon.zincrby('topcommit', cstatfull.author, cstatfull.total)
+        print (cstatfull.author)
+        print (str(cstatfull.author.login))
+        redcon.zincrby('r:{}'.format(r.name), cstatfull.total, cstatfull.author.login)
+        print (r.name," ", cstatfull.total , " ",  cstatfull.author.login)
+        redcon.zincrby('topcommit', cstatfull.total, cstatfull.author.login)
     for c in r.contributors():
         contributor = str(c)
         user = gh.user(contributor)
         redcon.set('a:{}'.format(user),user.avatar_url)
-        redcon.sadd('users', user)
-        redcon.zincrby('topversatile', contributor, 1)
+        redcon.sadd('users', user.login)
+        redcon.zincrby('topversatile', 1, contributor)
 
