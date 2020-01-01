7f2ec77e2f0d92585771b5c3abf5d6451ae89dc7
Alexandre Dulaunoy <a@foo.be>
2020-01-01 12:22:48 +0100

    chg: [auth] github username/password is now deprecated - replaced by token authentication

--- contributors.py
+++ contributors.py
@@ -1,12 +1,10 @@
 import github3
-import json
 import redis
-import time
 
-from config import username, password
-gh = github3.login(username, password=password)
+from config import username, token
+gh = github3.login(username, token=token)
 
-print (gh)
+print("Authenticated with {}".format(gh))
 contributors = {}
 
 redcon = redis.StrictRedis(host='localhost', port=6379, db=10, encoding=u'utf-8')
@@ -14,10 +12,10 @@ redcon.flushdb()
 
 for r in gh.repositories():
     if str(r.owner).find('MISP') >= 0:
-        print (r.owner)
+        print(r.owner)
         try:
             for x in r.contributor_statistics():
-                print ('trigger stats for {}'.format(r))
+                print('trigger stats for {}'.format(r))
         except:
             continue
         else:
@@ -29,15 +27,14 @@ for r in gh.repositories():
         continue
     redcon.sadd('repositories', r.name)
     for cstatfull in r.contributor_statistics():
-        print (cstatfull.author)
-        print (str(cstatfull.author.login))
+        print(cstatfull.author)
+        print(str(cstatfull.author.login))
         redcon.zincrby('r:{}'.format(r.name), cstatfull.total, cstatfull.author.login)
-        print (r.name," ", cstatfull.total , " ",  cstatfull.author.login)
+        print(r.name, " ", cstatfull.total, " ",  cstatfull.author.login)
         redcon.zincrby('topcommit', cstatfull.total, cstatfull.author.login)
     for c in r.contributors():
         contributor = str(c)
         user = gh.user(contributor)
-        redcon.set('a:{}'.format(user),user.avatar_url)
+        redcon.set('a:{}'.format(user), user.avatar_url)
         redcon.sadd('users', user.login)
         redcon.zincrby('topversatile', 1, contributor)
-
